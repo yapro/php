e359d7b5f073e9e0992c26909da54e652a23d55c
Lebnik <totx@yandex.ru>
2019-03-29 15:51:04 +0300

    Add files via upload

---
+++ User.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * The followings are the available columns in table 'tbl_user':
+ * @property integer $id
+ * @property string $username
+ * @property string $password
+ * @property string $email
+ * @property string $profile
+ */
+class User extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @return static the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'tbl_user';
+	}
+}
